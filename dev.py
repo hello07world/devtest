d7eadb97752632b28671a84c9a7d08099495b071
unknown <saimaliabrish@gmail.com>
2021-11-18 16:10:44 +0500

    first commit

---
+++ dev.py
@@ -0,0 +1,33 @@
+import requests
+# TESTING API DATA
+ALLIANZ_API_ENDPOINT = "https://apigw-dev.allianz.de/ldm/api/leads"
+ALLIANZ_ACCESS_TOKEN_ENDPOINT = (
+    "https://si-cim.allianz.de/auth/oauth2/realms/root/realms/eu1/access_token"
+)
+ALLIANZ_CLIENT_ID = "ppis1hqpgr2a7o9mks7kn6poih6vy83jbjbqh182"
+ALLIANZ_CLIENT_SECRET = "r9w58r4yg03rdqwfqk2i"
+
+
+def fetch_access_token():
+    response = requests.post(
+        (
+            f"{ALLIANZ_ACCESS_TOKEN_ENDPOINT}"
+            "?grant_type=client_credentials"
+            "&scope=clientauth"
+            f"&client_id={ALLIANZ_CLIENT_ID}"
+            f"&client_secret={ALLIANZ_CLIENT_SECRET}"
+        )
+    )
+
+    response = response.json()
+    access_token = response.get("access_token", False)
+
+    if access_token:
+        cache.set("access_token", access_token, 780)
+
+    return access_token
+
+
+access_token = fetch_access_token()
+
+print(access_token)
---
+++ generaterandomapi.py
@@ -0,0 +1,6 @@
+from faker import Factory
+
+fake_generator = Factory.create()
+print(fake_generator.ipv4())
+
+# get instgram user follower by username
